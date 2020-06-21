1a6c34176fde2cf6ac16e1453a64ee25156c6fa4
Milan <millan.djuric@hotmail.com>
2020-06-21 13:00:22 +0200

    Visualizing clusters with respect to first 2 principle components, pairplot for clusters over derived features

--- prvi zadatak/kmeans_pca.py
+++ prvi zadatak/kmeans_pca.py
@@ -3,6 +3,8 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sb
 
 def main():
     X = load_and_preprocess()
@@ -31,7 +33,9 @@ def main():
     #we chose 5 pcs as they explain 80% of the variance
     pca = PCA(n_components=5)
     reduced_dataframe = pca.fit_transform(X)
-    silhouette = []
+
+    #test number of clusters
+    """silhouette = []
     inertia = []
     for x in range(min_clusters, max_clusters):
         alg = KMeans(n_clusters=x)
@@ -52,8 +56,29 @@ def main():
     axes[1].set_ylabel("Cluster label")
     axes[1].plot(x, silhouette)
 
+    plt.show()"""
+
+    # according to plots, chose 4,5 or 6 clusters
+    #we chose 5 clusters
+    alg = KMeans(n_clusters=5)
+    label = alg.fit_predict(reduced_dataframe)
+
+    fig, ax = plt.subplots()
+    ax.set_title("5 clusters according to 5 principle components.")
+    ax.set_xlabel("PC 0")
+    ax.set_ylabel("PC 1")
+    plt.scatter(reduced_dataframe[:, 0], reduced_dataframe[:, 1], c=label.astype(float)) #visualize clustering according to first two prinicple components
     plt.show()
 
-    #according to plots, chose 4,5 or 6 clusters
+    chosen_columns = ["BALANCE_TO_CREDIT_LIMIT_RATIO", "PAYMENT_TO_MIN_PAYMENT_RATIO", "MONTHLY_AVERAGE_PURCHASES",
+                      "MONTHLY_AVERAGE_CASH_ADVANCE"]
+    X_chosen = pd.DataFrame(X[chosen_columns])
+    # create a 'cluster' column
+    X_chosen['CLUSTER_5'] = label
+    chosen_columns.append('CLUSTER_5')
+    # make a Seaborn pairplot
+    sb.pairplot(X_chosen, hue='CLUSTER_5', vars=chosen_columns, diag_kind='kde', size=3)
+    plt.show()
+
 if __name__ == '__main__':
     main()
